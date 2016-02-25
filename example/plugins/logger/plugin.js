af0dc57ccf0e1c530b4706b36259bebc6676ae0e
arildwtv <arild.wanvik@gmail.com>
2016-02-25 14:51:53 +0100

    Change some event names and occurrences

--- example/plugins/logger/plugin.js
+++ example/plugins/logger/plugin.js
@@ -1,27 +1,65 @@
+var directoriesCreated = [];
+var filesSkipped = [];
+var validationTime;
+var sourceGenerationTime;
+var sourceOutputTime;
+
 module.exports.subscriptions = () => {
     return [
         {
             event: 'directoryCreated',
             handler(dir) {
-                console.log('Directory created:', dir);
+                if (directoriesCreated.indexOf(dir) === -1) {
+                    directoriesCreated.push(dir);
+                }
             }
         },
         {
             event: 'fileCreated',
             handler(file) {
-                console.log('File created:', file.path);
+                console.log('File Created:', file.path);
             }
         },
         {
             event: 'fileSkipped',
             handler(file) {
-                console.log('File skipped:', file.path);
+                filesSkipped.push(file.path);
             }
         },
         {
             event: 'specInvalid',
             handler(err) {
                 console.log('Specification did not pass validation', err);
+            }
+        },
+        {
+            event: 'validationStarted',
+            handler(timeInMillis) {
+                console.log('Validation started...');
+                validationTime = timeInMillis;
+            }
+        },
+        {
+            event: 'validationCompleted',
+            handler(timeInMillis) {
+                console.log('Validation completed...');
+                validationTime = timeInMillis;
+            }
+        },
+        {
+            event: 'sourceGenerationCompleted',
+            handler(timeInMillis) {
+                console.log('Sources generated...');
+                sourceGenerationTime = timeInMillis;
+            }
+        },
+        {
+            event: 'sourceOutputCompleted',
+            handler(outputTime) {
+                console.log('\nFiles skipped:\n' + (filesSkipped.join('\n') || 'None'));
+                validationTime && console.log('\nSpecification validated in', validationTime, 'milliseconds');
+                console.log('\nSource generated in', sourceGenerationTime, 'milliseconds');
+                console.log('\nSource written to disk in', outputTime, 'milliseconds');
             }
         }
     ];
