0b10ba7ecccf7e0392a302375a81e6aa0cec89f4
arildwtv <arild.wanvik@gmail.com>
2016-04-12 17:30:53 +0200

    Use Promises instead of asyncawait to reduce dependencies.

--- example/plugins/rest/plugin.js
+++ example/plugins/rest/plugin.js
@@ -1,9 +1,6 @@
-var async = require('asyncawait/async');
-var await = require('asyncawait/await');
-
 var capitalize = require('../../../lib/util').capitalize;
 
-module.exports.generateSources = async ((spec, options, helper) => {
+module.exports.generateSources = (spec, options, helper) => {
 
     var interpolateFileAll = helper.interpolation.interpolateFileAll;
 
@@ -17,19 +14,20 @@ module.exports.generateSources = async ((spec, options, helper) => {
     var paramsList = mapServicesToTemplateParams(serviceNames, options);
 
     // Generate a rest controller per service, interpolating parameters.
-    var restControllerSources = await (interpolateFileAll(__dirname + '/tpl/RestService.tpl', paramsList));
+    return interpolateFileAll(__dirname + '/tpl/RestService.tpl', paramsList)
+        .then(restControllerSources => {
+            // Map rest controller sources to source definitions.
+            return restControllerSources.map((content, index) => {
+                var serviceName = serviceNames[index];
+                var path = options.package.replace(/\./g, '/') + '/' + serviceName.toLowerCase();
 
-    // Map rest controller sources to source definitions.
-    return restControllerSources.map((content, index) => {
-        var serviceName = serviceNames[index];
-        var path = options.package.replace(/\./g, '/') + '/' + serviceName.toLowerCase();
-
-        return {
-            path: path + '/' + capitalize(serviceName) + 'RestService.java',
-            content: content
-        };
-    });
-});
+                return {
+                    path: path + '/' + capitalize(serviceName) + 'RestService.java',
+                    content: content
+                };
+            });
+        });
+};
 
 function mapPathsToService(paths) {
     return Object.keys(paths).reduce((serviceMap, path) => {
